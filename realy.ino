24a972201c3469b7c574d7934b05ea1e84d225e6
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2021-10-13 22:42:41 +0530

    Add files via upload

---
+++ realy.ino
@@ -0,0 +1,30 @@
+#include <WiFi.h>
+const char* ssid = "Jio Home";
+const char* pass = "JH25021978";
+const uint16_t port = 8091;
+const char* host = "192.168.29.128";
+int led = 2;
+void setup(){
+    pinMode(led, OUTPUT);
+    Serial.begin(115200);
+    WiFi.begin(ssid, pass);
+     while (WiFi.status() != WL_CONNECTED) {
+        delay(600);
+        Serial.println("...Waitning for connection...");
+    }
+    Serial.println(WiFi.localIP());
+}
+void loop(){
+    WiFiClient client;
+    if(!client.connect(host, port)){
+        Serial.println("stopping...");
+        return;
+    }
+    client.println("We are friend right shourya");
+    client.read();
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led,LOW);
+    client.stop();
+    delay(10000);
+}
